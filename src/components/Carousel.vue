281f6a99b79cf19704e079356ddf7cf9006273f4
chexoov <151584367+chexoov@users.noreply.github.com>
2024-07-29 16:20:56 +0300

    Поработали с Emit , сделали адаптивный скрол

---
+++ src/components/Carousel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="m-10">
+    <el-carousel :interval="5000" arrow="always"
+    :style="`width: ${imagesWidth}vw`"
+    >
+      <el-carousel-item v-for="item in images" :key="item">
+        <img
+          :src="item.url"
+          alt="Изображение"
+          style="width: 100vh; height: auto"
+        />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+  <button @click="$emit('bruh', 'Hello Parent!')">тест эмитов</button>
+</template>
+
+<script setup>
+
+const props = defineProps(['images', 'imagesWidth']);
+
+
+</script>
